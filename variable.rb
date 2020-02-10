91a4ae000cbb07b8e1dee9244d3447c457bbf31c
decisive-reducer-5526 <norionorioc@gmail.com>
2020-02-10 19:11:18 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -2,3 +2,4 @@
  # give it the value of the string "Hello World"
 
 greeting = "Hello World"
+https://learn.co/tracks/online-software-engineering-structured/procedural-ruby/variables-and-methods/variable-assignment-lab#
